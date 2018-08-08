43b01e389963c06b5d9ae42e8cd5efd29172ac2a
JessYan <jessyan@foxmail.com>
2018-08-08 14:33:08 +0800

    Add ActivityLifecycleCallbacksImpl

---
+++ autosize/src/main/java/me/jessyan/autosize/ActivityLifecycleCallbacksImpl.java
@@ -0,0 +1,49 @@
+package me.jessyan.autosize;
+
+import android.app.Activity;
+import android.app.Application;
+import android.os.Bundle;
+
+/**
+ * ================================================
+ * Created by JessYan on 2018/8/8 14:32
+ * <a href="mailto:jess.yan.effort@gmail.com">Contact me</a>
+ * <a href="https://github.com/JessYanCoding">Follow me</a>
+ * ================================================
+ */
+public class ActivityLifecycleCallbacksImpl implements Application.ActivityLifecycleCallbacks {
+    @Override
+    public void onActivityCreated(Activity activity, Bundle savedInstanceState) {
+
+    }
+
+    @Override
+    public void onActivityStarted(Activity activity) {
+
+    }
+
+    @Override
+    public void onActivityResumed(Activity activity) {
+
+    }
+
+    @Override
+    public void onActivityPaused(Activity activity) {
+
+    }
+
+    @Override
+    public void onActivityStopped(Activity activity) {
+
+    }
+
+    @Override
+    public void onActivitySaveInstanceState(Activity activity, Bundle outState) {
+
+    }
+
+    @Override
+    public void onActivityDestroyed(Activity activity) {
+
+    }
+}
