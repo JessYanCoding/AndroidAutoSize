1350006dff4710e25b5373fe294075bec706f3e3
JessYan <jessyan@foxmail.com>
2018-08-08 10:10:24 +0800

    Init commit

---
+++ demo/src/main/java/me/jessyan/autosize/demo/MainActivity.java
@@ -0,0 +1,13 @@
+package me.jessyan.autosize.demo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
