d8c62b06c21092a4fbde264eb592e87396d2755c
JessYan <jessyan@foxmail.com>
2018-08-09 14:57:15 +0800

    Finish AutoSize

--- autosize/src/main/java/me/jessyan/autosize/AutoSize.java
+++ autosize/src/main/java/me/jessyan/autosize/AutoSize.java
@@ -15,12 +15,115 @@
  */
 package me.jessyan.autosize;
 
+import android.app.Activity;
+import android.util.DisplayMetrics;
+
+import me.jessyan.autosize.internal.CustomAdapt;
+import me.jessyan.autosize.utils.Preconditions;
+
 /**
  * ================================================
+ * AndroidAutoSize 用于屏幕适配的核心方法都在这里, 核心原理来自于 <a href="https://mp.weixin.qq.com/s/d9QCoBP6kV9VSWvVldVVwA">今日头条官方适配方案</a>
+ * <p>
  * Created by JessYan on 2018/8/8 19:20
  * <a href="mailto:jess.yan.effort@gmail.com">Contact me</a>
  * <a href="https://github.com/JessYanCoding">Follow me</a>
  * ================================================
  */
 public class AutoSize {
+
+    private AutoSize() {
+        throw new IllegalStateException("you can't instantiate me!");
+    }
+
+    /**
+     * 使用 AndroidAutoSize 初始化时设置的默认适配参数进行适配 (AndroidManifest 的 Meta 属性)
+     *
+     * @param activity {@link Activity}
+     */
+    public static void autoConvertDensityOfGlobal(Activity activity) {
+        if (AutoSizeConfig.getInstance().isBaseOnWidth()) {
+            autoConvertDensityBaseOnWidth(activity, AutoSizeConfig.getInstance().getDesignWidthInDp());
+        } else {
+            autoConvertDensityBaseOnHeight(activity, AutoSizeConfig.getInstance().getDesignHeightInDp());
+        }
+    }
+
+    /**
+     * 使用 {@link Activity} 的自定义参数进行适配
+     *
+     * @param activity    {@link Activity}
+     * @param customAdapt {@link Activity} 需实现 {@link CustomAdapt}
+     */
+    public static void autoConvertDensityOfCustomAdapt(Activity activity, CustomAdapt customAdapt) {
+        Preconditions.checkNotNull(customAdapt, "customAdapt == null");
+        float sizeInDp = customAdapt.getSizeInDp();
+
+        //如果 CustomAdapt#getSizeInDp() 返回 0, 则使用在 AndroidManifest 上填写的设计图尺寸
+        if (sizeInDp <= 0) {
+            if (customAdapt.isBaseOnWidth()) {
+                sizeInDp = AutoSizeConfig.getInstance().getDesignWidthInDp();
+            } else {
+                sizeInDp = AutoSizeConfig.getInstance().getDesignHeightInDp();
+            }
+        }
+        autoConvertDensity(activity, sizeInDp, customAdapt.isBaseOnWidth());
+    }
+
+    /**
+     * 以宽度为基准进行适配
+     *
+     * @param activity        {@link Activity}
+     * @param designWidthInDp 设计图的总宽度
+     */
+    public static void autoConvertDensityBaseOnWidth(Activity activity, float designWidthInDp) {
+        autoConvertDensity(activity, designWidthInDp, true);
+    }
+
+    /**
+     * 以高度为基准进行适配
+     *
+     * @param activity         {@link Activity}
+     * @param designHeightInDp 设计图的总高度
+     */
+    public static void autoConvertDensityBaseOnHeight(Activity activity, float designHeightInDp) {
+        autoConvertDensity(activity, designHeightInDp, false);
+    }
+
+    /**
+     * 这里是今日头条适配方案的核心代码, 核心在于根据当前设备的实际情况做自动计算并转换 {@link DisplayMetrics#density}、
+     * {@link DisplayMetrics#scaledDensity}、{@link DisplayMetrics#densityDpi} 这三个值, 有兴趣请看下面的链接
+     *
+     * @param activity      {@link Activity}
+     * @param sizeInDp      设计图上的设计尺寸, 单位 dp, 如果 {@param isBaseOnWidth} 设置为 {@code true},
+     *                      {@param sizeInDp} 则应该填写设计图的总宽度, 如果 {@param isBaseOnWidth} 设置为 {@code false},
+     *                      {@param sizeInDp} 则应该填写设计图的总高度
+     * @param isBaseOnWidth 是否按照宽度进行等比例适配, {@code true} 为以宽度进行等比例适配, {@code false} 为以高度进行等比例适配
+     * @see <a href="https://mp.weixin.qq.com/s/d9QCoBP6kV9VSWvVldVVwA">今日头条官方适配方案</a>
+     */
+    public static void autoConvertDensity(Activity activity, float sizeInDp, boolean isBaseOnWidth) {
+        Preconditions.checkNotNull(activity, "activity == null");
+        float targetDensity;
+        if (isBaseOnWidth) {
+            targetDensity = AutoSizeConfig.getInstance().getScreenWidth() * 1.0f / sizeInDp;
+        } else {
+            targetDensity = AutoSizeConfig.getInstance().getScreenHeight() * 1.0f / sizeInDp;
+        }
+        final float targetScaledDensity = targetDensity * (AutoSizeConfig.getInstance().getInitScaledDensity() * 1.0f / AutoSizeConfig.getInstance().getInitDensity());
+        final int targetDensityDpi = (int) (targetDensity * 160);
+
+        //Application
+        final DisplayMetrics appDisplayMetrics = AutoSizeConfig.getInstance().getApplication().getResources().getDisplayMetrics();
+
+        appDisplayMetrics.density = targetDensity;
+        appDisplayMetrics.densityDpi = targetDensityDpi;
+        appDisplayMetrics.scaledDensity = targetScaledDensity;
+
+        //Activity
+        final DisplayMetrics activityDisplayMetrics = activity.getResources().getDisplayMetrics();
+
+        activityDisplayMetrics.density = targetDensity;
+        activityDisplayMetrics.densityDpi = targetDensityDpi;
+        activityDisplayMetrics.scaledDensity = targetScaledDensity;
+    }
 }
