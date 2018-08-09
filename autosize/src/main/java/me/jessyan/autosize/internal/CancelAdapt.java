2a8da024bf814e3fb6106bef01996403595ca2c7
JessYan <jessyan@foxmail.com>
2018-08-09 15:24:30 +0800

    Improve CancelAdapt's comments

--- autosize/src/main/java/me/jessyan/autosize/internal/CancelAdapt.java
+++ autosize/src/main/java/me/jessyan/autosize/internal/CancelAdapt.java
@@ -19,7 +19,7 @@ import android.app.Activity;
 
 /**
  * ================================================
- * AndroidAutoSize 默认项目中的所有模块都使用适配功能, 三方库也不例外
+ * AndroidAutoSize 默认项目中的所有模块都使用适配功能, 三方库的 {@link Activity} 也不例外
  * 如果某个页面不想使用适配功能, 请让该页面 {@link Activity} 实现此接口
  * 实现此接口表示放弃适配, 所有的适配效果都将失效
  * <p>
