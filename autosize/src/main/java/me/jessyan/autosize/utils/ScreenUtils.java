1fd7ef963e659481914684c1f7d91632d5d0b57f
JessYan <jessyan@foxmail.com>
2018-08-11 11:05:41 +0800

    Improve ScreenUtils#getStatusBarHeight()

--- autosize/src/main/java/me/jessyan/autosize/utils/ScreenUtils.java
+++ autosize/src/main/java/me/jessyan/autosize/utils/ScreenUtils.java
@@ -32,12 +32,12 @@ import android.view.WindowManager;
  */
 public class ScreenUtils {
 
-    public static int getStatusBarHeight(Context context) {
+    public static int getStatusBarHeight() {
         int result = 0;
         try {
-            int resourceId = context.getResources().getIdentifier("status_bar_height", "dimen", "android");
+            int resourceId = Resources.getSystem().getIdentifier("status_bar_height", "dimen", "android");
             if (resourceId > 0) {
-                result = context.getResources().getDimensionPixelSize(resourceId);
+                result = Resources.getSystem().getDimensionPixelSize(resourceId);
             }
         } catch (Resources.NotFoundException e) {
             e.printStackTrace();
