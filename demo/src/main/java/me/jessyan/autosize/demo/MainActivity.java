5c132ef2c9ef97a8d783ee726db85fddd8fb26e1
JessYan <jessyan@foxmail.com>
2018-08-16 11:39:30 +0800

    Improve MainActivity's comments

--- demo/src/main/java/me/jessyan/autosize/demo/MainActivity.java
+++ demo/src/main/java/me/jessyan/autosize/demo/MainActivity.java
@@ -29,8 +29,8 @@ import me.jessyan.autosize.internal.CustomAdapt;
  * ================================================
  * 本框架核心原理来自于 <a href="https://mp.weixin.qq.com/s/d9QCoBP6kV9VSWvVldVVwA">今日头条官方适配方案</a>
  * <p>
- * {@link MainActivity} 是以屏幕宽度为基准进行适配的, 不懂什么叫基准的话, 请看 {@link AutoSizeConfig#isBaseOnWidth}) 的注释
- * AndroidAutoSize 默认全局以屏幕宽度为基准进行适配
+ * {@link MainActivity} 是以屏幕宽度为基准进行适配的, 并且使用的是在 AndroidManifest 中填写的全局设计图尺寸 360 * 640
+ * 不懂什么叫基准的话, 请看 {@link AutoSizeConfig#isBaseOnWidth}) 的注释, AndroidAutoSize 默认全局以屏幕宽度为基准进行适配
  * 如果想更改为全局以屏幕高度为基准进行适配, 请在 {@link BaseApplication} 中按注释中更改, 为什么强调全局？
  * 因为 AndroidAutoSize 允许每个 {@link Activity} 可以自定义适配参数, 自定义适配参数通过实现 {@link CustomAdapt}
  * 如果不自定义适配参数就会使用全局的适配参数, 全局适配参数在 {@link BaseApplication} 中按注释设置
