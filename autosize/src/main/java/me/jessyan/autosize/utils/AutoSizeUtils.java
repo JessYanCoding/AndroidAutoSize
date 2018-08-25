cfd0f55a12035c2e10c6cc55084e460ee40b07f7
JessYan <jessyan@foxmail.com>
2018-08-25 15:49:30 +0800

    Create AutoSizeUtils

---
+++ autosize/src/main/java/me/jessyan/autosize/utils/AutoSizeUtils.java
@@ -0,0 +1,43 @@
+/*
+ * Copyright 2018 JessYan
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package me.jessyan.autosize.utils;
+
+import android.content.Context;
+import android.util.TypedValue;
+
+/**
+ * ================================================
+ * AndroidAutoSize 常用工具类
+ * <p>
+ * Created by JessYan on 2018/8/25 15:24
+ * <a href="mailto:jess.yan.effort@gmail.com">Contact me</a>
+ * <a href="https://github.com/JessYanCoding">Follow me</a>
+ * ================================================
+ */
+public class AutoSizeUtils {
+
+    private AutoSizeUtils() {
+        throw new IllegalStateException("you can't instantiate me!");
+    }
+
+    public static int dp2px(Context context, float value) {
+        return (int) (TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, value, context.getResources().getDisplayMetrics()) + 0.5f);
+    }
+
+    public static int sp2px(Context context, float value) {
+        return (int) (TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_SP, value, context.getResources().getDisplayMetrics()) + 0.5f);
+    }
+}
