0d8c6a0e4a1c73dcc589b90f7e6154052d31296a
JessYan <jessyan@foxmail.com>
2018-08-28 17:27:35 +0800

    Update design size

--- demo-subunits/src/main/java/me/jessyan/autosize/demo/subunits/BaseApplication.java
+++ demo-subunits/src/main/java/me/jessyan/autosize/demo/subunits/BaseApplication.java
@@ -37,31 +37,6 @@ public class BaseApplication extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
-        /**
-         * 以下是 AndroidAutoSize 可以自定义的参数, {@link AutoSizeConfig} 的每个方法的注释都写的很详细
-         * 使用前请一定记得跳进源码，查看方法的注释, 下面的注释只是简单描述!!!
-         */
-        AutoSizeConfig.getInstance()
-
-                //是否让框架支持自定义 Fragment 的适配参数, 由于这个需求是比较少见的, 所以须要使用者手动开启
-                //如果没有这个需求建议不开启
-                .setCustomFragment(true)
-
-                //是否打印 AutoSize 的内部日志, 默认为 true, 如果您不想 AutoSize 打印日志, 则请设置为 false
-//                .setLog(false)
-
-                //是否使用设备的实际尺寸做适配, 默认为 false, 如果设置为 false, 在以屏幕高度为基准进行适配时
-                //AutoSize 会将屏幕总高度减去状态栏高度来做适配, 如果设备上有导航栏还会减去导航栏的高度
-                //设置为 true 则使用设备的实际屏幕高度, 不会减去状态栏以及导航栏高度
-//                .setUseDeviceSize(true)
-
-                //是否全局按照宽度进行等比例适配, 默认为 true, 如果设置为 false, AutoSize 会全局按照高度进行适配
-//                .setBaseOnWidth(false)
-
-                 //设置屏幕适配逻辑策略类, 一般不用设置, 使用框架默认的就好
-//                .setAutoAdaptStrategy(new AutoAdaptStrategy())
-        ;
-
         AutoSizeConfig.getInstance().getUnitsManager().setSupportDP(false);
         AutoSizeConfig.getInstance().getUnitsManager().setSupportSP(false);
         AutoSizeConfig.getInstance().getUnitsManager().setSupportSubunits(Subunits.MM);
