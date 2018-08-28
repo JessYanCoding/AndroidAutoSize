dde85630db27df7e4d0731d7519888d3ac4cb0bd
JessYan <jessyan@foxmail.com>
2018-08-28 18:59:13 +0800

    Improve subunits/MainActivity

--- demo-subunits/src/main/java/me/jessyan/autosize/demo/subunits/MainActivity.java
+++ demo-subunits/src/main/java/me/jessyan/autosize/demo/subunits/MainActivity.java
@@ -16,34 +16,20 @@
 package me.jessyan.autosize.demo.subunits;
 
 import android.app.Activity;
-import android.app.Dialog;
-import android.app.Fragment;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 
-import me.jessyan.autosize.AutoSizeConfig;
-import me.jessyan.autosize.internal.CustomAdapt;
-
 /**
  * ================================================
  * 本框架核心原理来自于 <a href="https://mp.weixin.qq.com/s/d9QCoBP6kV9VSWvVldVVwA">今日头条官方适配方案</a>
- * 此方案不光可以适配 {@link Activity}, 这个 {@link Activity} 下的所有 {@link Fragment}、{@link Dialog}、{@link View} 都会自动适配
- * <p>
- * {@link MainActivity} 是以屏幕宽度为基准进行适配的, 并且使用的是在 AndroidManifest 中填写的全局设计图尺寸 360 * 640
- * 不懂什么叫基准的话, 请看 {@link AutoSizeConfig#isBaseOnWidth}) 的注释, AndroidAutoSize 默认全局以屏幕宽度为基准进行适配
- * 如果想更改为全局以屏幕高度为基准进行适配, 请在 {@link BaseApplication} 中按注释中更改, 为什么强调全局？
- * 因为 AndroidAutoSize 允许每个 {@link Activity} 可以自定义适配参数, 自定义适配参数通过实现 {@link CustomAdapt}
- * 如果不自定义适配参数就会使用全局的适配参数, 全局适配参数在 {@link BaseApplication} 中按注释设置
  * <p>
  * Created by JessYan on 2018/8/9 17:05
  * <a href="mailto:jess.yan.effort@gmail.com">Contact me</a>
  * <a href="https://github.com/JessYanCoding">Follow me</a>
  * ================================================
  */
-//实现 CancelAdapt 即可取消当前 Activity 的屏幕适配, 并且这个 Activity 下的所有 Fragment 和 View 都会被取消适配
-//public class MainActivity extends AppCompatActivity implements CancelAdapt {
 public class MainActivity extends AppCompatActivity {
 
     @Override
