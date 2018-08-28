11e6dfaf47ce1e13ab9eaf13e47629ede0cb4f65
JessYan <jessyan@foxmail.com>
2018-08-28 18:42:51 +0800

    Finish subunits/BaseApplication.java

--- demo-subunits/src/main/java/me/jessyan/autosize/demo/subunits/BaseApplication.java
+++ demo-subunits/src/main/java/me/jessyan/autosize/demo/subunits/BaseApplication.java
@@ -16,9 +16,11 @@
 package me.jessyan.autosize.demo.subunits;
 
 import android.app.Application;
+import android.util.DisplayMetrics;
 
 import me.jessyan.autosize.AutoSizeConfig;
 import me.jessyan.autosize.unit.Subunits;
+import me.jessyan.autosize.unit.UnitsManager;
 
 /**
  * ================================================
@@ -37,8 +39,44 @@ public class BaseApplication extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
-        AutoSizeConfig.getInstance().getUnitsManager().setSupportDP(false);
-        AutoSizeConfig.getInstance().getUnitsManager().setSupportSP(false);
-        AutoSizeConfig.getInstance().getUnitsManager().setSupportSubunits(Subunits.MM);
+        //对单位的自定义配置, 请在 App 启动时完成 
+        configUnits();
+    }
+
+    /**
+     * v0.9.0 以后, AndroidAutoSize 强势升级, 将这个方案做到极致, 现在支持5种单位 (dp、sp、pt、in、mm)
+     * {@link UnitsManager} 可以让使用者随意配置自己想使用的单位类型
+     * 其中 dp、sp 这两个是比较常见的单位, 作为 AndroidAutoSize 的主单位, 默认被 AndroidAutoSize 支持
+     * pt、in、mm 这三个是比较少见的单位, 只可以选择其中的一个, 作为 AndroidAutoSize 的副单位, 与 dp、sp 一起被 AndroidAutoSize 支持
+     * 副单位是用于规避修改 {@link DisplayMetrics#density} 所带来的对于其他使用 dp 布局的系统控件或三方库控件的不良影响
+     * 您选择什么单位, 就在 layout 文件中用什么单位布局
+     * <p>
+     * 两个主单位和一个副单位, 可以随时使用 {@link UnitsManager} 的方法关闭和重新开启对它们的支持
+     * 如果您想完全规避修改 {@link DisplayMetrics#density} 所带来的对于其他使用 dp 布局的系统控件或三方库控件的不良影响
+     * 那请调用 {@link UnitsManager#setSupportDP}、{@link UnitsManager#setSupportSP} 都设置为 {@code false}
+     * 停止对两个主单位的支持 (如果开启 sp, 对其他三方库控件影响不大, 也可以不关闭对 sp 的支持)
+     * 并调用 {@link UnitsManager#setSupportSubunits} 从三个冷门单位中选择一个作为副单位
+     * 三个单位的效果都是一样的, 按自己的喜好选择, 比如我就喜欢 mm, 翻译为中文是妹妹的意思
+     * 然后在 layout 文件中只使用这个副单位进行布局, 这样就可以完全规避修改 {@link DisplayMetrics#density} 所带来的不良影响
+     * 因为 dp、sp 这两个单位在其他系统控件或三方库控件中都非常常见, 但三个冷门单位却非常少见
+     */
+    private void configUnits() {
+        //AndroidAutoSize 默认开启对 dp 的支持, 调用 UnitsManager.setSupportDP(false); 可以关闭对 dp 的支持
+        //主单位 dp 和 副单位可以同时开启的原因是, 对于旧项目中已经使用了 dp 进行布局的页面的兼容
+        //让开发者的旧项目可以渐进式的从 dp 切换到副单位, 即新页面用副单位进行布局, 然后抽时间逐渐的将旧页面的布局单位从 dp 改为副单位
+        //最后将 dp 全部改为副单位后, 再使用 UnitsManager.setSupportDP(false); 将 dp 的支持关闭, 彻底隔离修改 density 所带来的不良影响
+        //如果项目完全使用副单位, 则可以直接以像素为单位填写 AndroidManifest 中需要填写的设计图尺寸, 不需再把像素转化为 dp
+        AutoSizeConfig.getInstance().getUnitsManager()
+                .setSupportDP(false)
+
+                //AndroidAutoSize 默认开启对 sp 的支持, 调用 UnitsManager.setSupportSP(false); 可以关闭对 sp 的支持
+                //如果开启 sp, 对其他三方库控件影响不大, 也可以不关闭对 sp 的支持, 这里我就继续开启 sp, 请自行斟酌自己的项目是否需要关闭对 sp 的支持
+//                .setSupportSP(false)
+
+                //AndroidAutoSize 默认不支持副单位, 调用 UnitsManager#setSupportSubunits() 可选择一个自己心仪的副单位, 并开启对副单位的支持
+                //只能在 pt、in、mm 这三个冷门单位中选择一个作为副单位, 三个单位的适配效果其实都是一样的, 您觉的哪个单位看起顺眼就用哪个
+                //您选择什么单位就在 layout 文件中用什么单位进行布局, 我选择用 mm 为单位进行布局, 因为 mm 翻译为中文是妹妹的意思
+                //如果大家生活中没有妹妹, 那我们就让项目中最不缺的就是妹妹!
+                .setSupportSubunits(Subunits.MM);
     }
 }
