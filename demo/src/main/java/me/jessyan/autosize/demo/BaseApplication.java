04870f72fbaf6ad8f589d29f93f3fef0f0642945
JessYan <jessyan@foxmail.com>
2018-11-09 17:02:16 +0800

    Support Multi-Window

--- demo/src/main/java/me/jessyan/autosize/demo/BaseApplication.java
+++ demo/src/main/java/me/jessyan/autosize/demo/BaseApplication.java
@@ -66,6 +66,10 @@ public class BaseApplication extends Application {
                 .setOnAdaptListener(new onAdaptListener() {
                     @Override
                     public void onAdaptBefore(Object target, Activity activity) {
+                        //使用以下代码, 可支持 Android 的分屏或缩放模式, 但前提是在分屏或缩放模式下当用户改变您 App 的窗口大小时
+                        //系统会重绘当前的页面, 经测试在某些机型, 某些情况下系统不会重绘当前页面, ScreenUtils.getScreenSize(activity) 的参数一定要不要传 Application!!!
+//                        AutoSizeConfig.getInstance().setScreenWidth(ScreenUtils.getScreenSize(activity)[0]);
+//                        AutoSizeConfig.getInstance().setScreenHeight(ScreenUtils.getScreenSize(activity)[1]);
                         LogUtils.d(String.format(Locale.ENGLISH, "%s onAdaptBefore!", target.getClass().getName()));
                     }
 
