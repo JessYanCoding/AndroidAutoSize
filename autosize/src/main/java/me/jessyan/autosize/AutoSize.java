b27429bd4235378fcad84b5734ffa584f1262376
JessYan <jessyan@foxmail.com>
2018-11-22 09:54:05 +0800

    Improve AutoSize#autoConvertDensity

--- autosize/src/main/java/me/jessyan/autosize/AutoSize.java
+++ autosize/src/main/java/me/jessyan/autosize/AutoSize.java
@@ -148,9 +148,13 @@ public final class AutoSize {
     public static void autoConvertDensity(Activity activity, float sizeInDp, boolean isBaseOnWidth) {
         Preconditions.checkNotNull(activity, "activity == null");
 
+        float subunitsDesignSize = isBaseOnWidth ? AutoSizeConfig.getInstance().getUnitsManager().getDesignWidth()
+                : AutoSizeConfig.getInstance().getUnitsManager().getDesignHeight();
+        subunitsDesignSize = subunitsDesignSize > 0 ? subunitsDesignSize : sizeInDp;
+
         int screenSize = isBaseOnWidth ? AutoSizeConfig.getInstance().getScreenWidth()
                 : AutoSizeConfig.getInstance().getScreenHeight();
-        String key = sizeInDp + "|" + isBaseOnWidth + "|"
+        String key = sizeInDp + "|" + subunitsDesignSize + "|" + isBaseOnWidth + "|"
                 + AutoSizeConfig.getInstance().isUseDeviceSize() + "|"
                 + AutoSizeConfig.getInstance().getInitScaledDensity() + "|"
                 + screenSize;
@@ -173,9 +177,9 @@ public final class AutoSize {
             targetDensityDpi = (int) (targetDensity * 160);
 
             if (isBaseOnWidth) {
-                targetXdpi = AutoSizeConfig.getInstance().getScreenWidth() * 1.0f / sizeInDp;
+                targetXdpi = AutoSizeConfig.getInstance().getScreenWidth() * 1.0f / subunitsDesignSize;
             } else {
-                targetXdpi = AutoSizeConfig.getInstance().getScreenHeight() * 1.0f / sizeInDp;
+                targetXdpi = AutoSizeConfig.getInstance().getScreenHeight() * 1.0f / subunitsDesignSize;
             }
 
             mCache.put(key, new DisplayMetricsInfo(targetDensity, targetDensityDpi, targetScaledDensity, targetXdpi));
@@ -188,9 +192,10 @@ public final class AutoSize {
 
         setDensity(activity, targetDensity, targetDensityDpi, targetScaledDensity, targetXdpi);
 
-        LogUtils.d(String.format(Locale.ENGLISH, "The %s has been adapted! \n%s Info: isBaseOnWidth = %s, %s = %f, targetDensity = %f, targetScaledDensity = %f, targetDensityDpi = %d, targetXdpi = %f"
+        LogUtils.d(String.format(Locale.ENGLISH, "The %s has been adapted! \n%s Info: isBaseOnWidth = %s, %s = %f, %s = %f, targetDensity = %f, targetScaledDensity = %f, targetDensityDpi = %d, targetXdpi = %f"
                 , activity.getClass().getName(), activity.getClass().getSimpleName(), isBaseOnWidth, isBaseOnWidth ? "designWidthInDp"
-                        : "designHeightInDp", sizeInDp, targetDensity, targetScaledDensity, targetDensityDpi, targetXdpi));
+                        : "designHeightInDp", sizeInDp, isBaseOnWidth ? "designWidthInSubunits" : "designHeightInSubunits", subunitsDesignSize
+                , targetDensity, targetScaledDensity, targetDensityDpi, targetXdpi));
     }
 
     /**
